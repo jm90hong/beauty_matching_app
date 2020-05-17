4ad5ef6a515ce6cee9dc55d0bec6d533f4d29558
user <jm90.hong@gmail.com>
2020-05-17 17:46:08 +0900

    4

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,14 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-
 class SplashScreen extends StatefulWidget {
   static const id='splash';
-
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
+
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
@@ -22,7 +21,7 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('가다나다라마바사',style: TextStyle(fontFamily: 'ng',fontSize: 30,color: Colors.red),)
+            Text('',style: TextStyle(fontFamily:'ng',fontSize:30,color: Colors.red),)
           ],
         ),
       ),
